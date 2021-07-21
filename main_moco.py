31d05e4386b51cb3b21a0e8758671a4ac37f95b3
KinanZ <kinan.z101@gmail.com>
2021-07-21 20:03:10 +0200

    accuracy prob

--- main_moco.py
+++ main_moco.py
@@ -427,6 +427,7 @@ def accuracy(output, target, topk=(1,)):
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
+        print('pred_shape', pred.shape)
         print('correct_shape: ', correct.shape)
 
         res = []
