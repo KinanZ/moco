4454a2553e43ea96814a45390710132071655e98
KinanZ <kinan.z101@gmail.com>
2021-07-21 19:33:09 +0200

    fixed the dataset class

--- CT_scans_dataset.py
+++ CT_scans_dataset.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset
 class brain_CT_scan(Dataset):
     """Brain CT Scans dataset."""
 
-    def __init__(self, json_file, root_dir, transform=None, num_channels=3, num_classes=15):
+    def __init__(self, json_file, root_dir, transform=None, num_channels=3):
         """
         Args:
             json_file (string): Path to the json file with annotations.
@@ -19,13 +19,11 @@ class brain_CT_scan(Dataset):
                 on a sample.
             num_channels (int): if 1 -> one slice is the input for the model,
                 if 3 -> the previous and post slices are stacked to the main slice and become a 3-channel input for the model.
-            num_classes (int): number of categories in the dataset
         """
         with open(json_file) as f_obj:
             self.dataset_annotations = json.load(f_obj)["questions"]
         self.root_dir = root_dir
         self.transform = transform
-        self.num_classes = num_classes
         self.num_channels = num_channels
 
     def __len__(self):
@@ -61,14 +59,7 @@ class brain_CT_scan(Dataset):
             image = np.dstack((image_pre, image_mid, image_post))
             image = torch.from_numpy(image).permute(2, 0, 1)
 
-        classes = self.dataset_annotations[idx]['labels']
-        labels = np.zeros(self.num_classes).astype(np.uint8)
-        labels[classes] = 1
+        image1 = self.transform(image)
+        image2 = self.transform(image)
 
-        if self.transform:
-            image = self.transform(image)
-
-        return {
-            'image': image,
-            'labels': torch.tensor(labels, dtype=torch.float32)
-        }
+        return (image1, image2), 0
