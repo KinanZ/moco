bc615d9b0945274689f1c6d37f0ee2ae47d3ee16
KinanZ <kinan.z101@gmail.com>
2021-07-28 14:47:22 +0200

    debugging multi process

--- job_bash_moco_test.sh
+++ job_bash_moco_test.sh
@@ -1,4 +1,4 @@
-#PBS -N moco_1c_v1
+#PBS -N test
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin11GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
@@ -13,12 +13,12 @@ echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], tim
 nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
 
 echo 'Training Should start'
-python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco.py \
-  --exp '1c_v1_2' \
-  --epochs 200 \
-  --print-freq 10 \
+python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco_old.py \
+  --exp 'test' \
+  --epochs 10 \
+  --print-freq 2 \
   --arch resnet50 \
-  --channel_num 1 \
+  --channel_num 3 \
   --lr 0.03 \
   --batch-size 32 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
