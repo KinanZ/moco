126db93f31a8f1dae499595bf24fc836fbdec88f
KinanZ <kinan.z101@gmail.com>
2021-07-29 19:43:48 +0200

    moco and linear classification

--- job_bash_moco_3c_v2_bestAug_GB.sh
+++ job_bash_moco_3c_v2_bestAug_GB.sh
@@ -15,7 +15,7 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco.py \
   --exp '3c_v2_bestAug_GB' \
-  --epochs 64 \
+  --epochs 3 \
   --print-freq 10 \
   --arch resnet50 \
   --num_channels 3 \
