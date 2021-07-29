5e974ec1c846cfaf868704c8a1e5bf10cb3b4fe7
KinanZ <kinan.z101@gmail.com>
2021-07-29 13:21:23 +0200

    is it too slow

--- job_bash_moco_3c_v2_bestAug_GB.sh
+++ job_bash_moco_3c_v2_bestAug_GB.sh
@@ -20,11 +20,11 @@ python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco
   --arch resnet50 \
   --channel_num 3 \
   --lr 0.03 \
-  --batch-size 48 \
+  --batch-size 46 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --mlp \
   --moco-t 0.2 \
-  --moco-k 64992 \
+  --moco-k 64952 \
   --cos \
   --workers 8 \
   --gbp 0.5 \
