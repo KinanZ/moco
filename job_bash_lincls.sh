b94ca3791bf67c884802138a8825b252e62e261f
KinanZ <kinan.z101@gmail.com>
2021-07-30 10:51:14 +0200

    moco and linear classification

--- job_bash_lincls.sh
+++ job_bash_lincls.sh
@@ -14,12 +14,13 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_lincls.py \
-  --exp 'test_1' \
-  --epochs 2 \
+  --exp 'test_2' \
+  --epochs 40 \
   --print-freq 1 \
   --arch resnet50 \
   --num_channels 3 \
   --lr 30.0 \
+  --schedule [20, 30] \
   --batch-size 32 \
   --pretrained /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs/3c_v2_bestAug/checkpoint_0063.pth.tar \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
