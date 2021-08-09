493a6f8ae0295477de36c4949686bb53bc5dd44b
KinanZ <kinan.z101@gmail.com>
2021-08-10 00:26:34 +0200

    finetune linear classifier

---
+++ job_bash_lincls_all_no_mlp.sh
@@ -0,0 +1,27 @@
+#PBS -N moco_lincls_all_upgraded_no_mlp
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin11GB,mem=16gb,walltime=24:00:00
+#PBS -j oe
+#PBS -q student
+#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs_lincls/
+
+
+homePath='/misc/student/alzouabk/miniconda3'
+source $homePath/bin/activate Thesis_CT_scans
+
+echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], time [ms]"
+nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
+
+echo 'Training Should start'
+python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_lincls.py \
+  --exp 'bestAug_lincls_all_upgraded_no_mlp' \
+  --epochs 40 \
+  --print-freq 1 \
+  --arch resnet50 \
+  --num_channels 3 \
+  --lr 0.0375 \
+  --batch-size 32 \
+  --pretrained /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs/3c_v2_bestAug_all_upgraded_no_mlp/best_model.pth.tar \
+  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --workers 8 \
+
