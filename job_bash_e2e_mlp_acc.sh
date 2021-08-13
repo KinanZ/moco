c0faed3118b32bd1c3064da8c00ae390d4bb7196
KinanZ <kinan.z101@gmail.com>
2021-08-13 14:03:50 +0200

    e2e finetuning

--- job_bash_e2e_mlp_acc.sh
+++ job_bash_e2e_mlp_acc.sh
@@ -1,4 +1,4 @@
-#PBS -N moco_e2e_no_mlp_acc
+#PBS -N moco_e2e_mlp_acc
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
@@ -14,13 +14,13 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_lincls.py \
-  --exp 'moco_e2e_no_mlp_acc' \
+  --exp 'moco_e2e_mlp_acc' \
   --epochs 40 \
   --e2e True \
   --print-freq 1 \
   --arch resnet18 \
   --lr 0.001 \
   --batch-size 48 \
-  --pretrained misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs/no_mlp_bestAug/best_model_acc.pth.tar \
+  --pretrained misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs/mlp_bestAug/best_model_acc.pth.tar \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --workers 8 \
