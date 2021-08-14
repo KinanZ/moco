31cd935582e74b3a1018cacab14482dedb7dee99
KinanZ <kinan.z101@gmail.com>
2021-08-14 20:28:56 +0200

    e2e debug

---
+++ job_bash_e2e_debug.sh
@@ -0,0 +1,27 @@
+#PBS -N moco_e2e_debug
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin11GB,mem=16gb,walltime=24:00:00
+#PBS -j oe
+#PBS -q student
+#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs_lincls/
+
+
+homePath='/misc/student/alzouabk/miniconda3'
+source $homePath/bin/activate Thesis_CT_scans
+
+echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], time [ms]"
+nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
+
+echo 'Training Should start'
+python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_lincls.py \
+  --exp 'moco_e2e_debug' \
+  --epochs 5 \
+  --e2e True \
+  --optimizer adam \
+  --print-freq 1 \
+  --arch resnet18 \
+  --lr 0.0005 \
+  --batch-size 48 \
+  --pretrained /misc/student/alzouabk/Thesis/supervised_multi_label_classification/outputs_final/supervised_MLC_affine_h_ED_s_%_resnet_18/model.pth \
+  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --workers 8 \
