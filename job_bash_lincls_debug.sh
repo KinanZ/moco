ad2a38776c5efa6be62e46856b6fc8c8cd9c58e8
KinanZ <kinan.z101@gmail.com>
2021-08-19 12:20:07 +0200

    another debug

--- job_bash_lincls_debug.sh
+++ job_bash_lincls_debug.sh
@@ -1,4 +1,4 @@
-#PBS -N imagenet_lincl_old
+#PBS -N imagenet_lincl_old_2
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin11GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
@@ -14,11 +14,11 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_lincls.py \
-  --exp 'imagenet_lincl_old' \
+  --exp 'imagenet_lincl_old_2' \
   --epochs 40 \
   --print-freq 1 \
   --arch resnet50 \
-  --lr 0.01 \
+  --lr 0.375 \
   --batch-size 48 \
   --pretrained /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs/3c_v2_bestAug_2_128/checkpoint_0099.pth.tar \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
