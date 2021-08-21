c4301a52e2cf2a9117a3a6836ad3a15a4b8d0862
KinanZ <kinan.z101@gmail.com>
2021-08-21 14:18:41 +0200

    deep exp

---
+++ job_bash_moco_mlp_RN50.sh
@@ -0,0 +1,29 @@
+#PBS -N moco_mlp_RN50_01
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=4:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
+#PBS -j oe
+#PBS -q student
+#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/moco_curves_2/
+
+
+homePath='/misc/student/alzouabk/miniconda3'
+source $homePath/bin/activate Thesis_CT_scans
+
+echo "pid, gpu_utilization [%], mem_utilization [%], max_memory_usage [MiB], time [ms]"
+nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" --format=csv | tail -n1
+
+echo 'Training Should start'
+python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco.py \
+  --exp 'mlp_RN50_01' \
+  --epochs 100 \
+  --print-freq 10 \
+  --arch resnet18 \
+  --moco-dim 128 \
+  --stack_pre_post True \
+  --lr 0.01 \
+  --batch-size 48 \
+  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --mlp \
+  --moco-t 0.2 \
+  --workers 8 \
+
--- job_bash_moco_mlp_RN50_cos.sh
+++ job_bash_moco_mlp_RN50_cos.sh
@@ -1,9 +1,9 @@
-#PBS -N moco_mlp_bestAug
+#PBS -N moco_mlp_RN50_cos_01
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=8:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
 #PBS -q student
-#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/moco_curves/
+#PBS -o /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/moco_curves_2/
 
 
 homePath='/misc/student/alzouabk/miniconda3'
@@ -14,13 +14,13 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco.py \
-  --exp 'mlp_bestAug' \
+  --exp 'mlp_RN50_cos_01' \
   --epochs 100 \
   --print-freq 10 \
   --arch resnet18 \
   --moco-dim 128 \
   --stack_pre_post True \
-  --lr 0.00375 \
+  --lr 0.01 \
   --batch-size 48 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --mlp \
