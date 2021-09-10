e2ba6a3dd0b9e0a90d46baf0a94c27bc571dc865
KinanZ <kinan.z101@gmail.com>
2021-09-10 17:39:07 +0200

    more exps

--- job_bash_moco_RN50.sh
+++ job_bash_moco_RN50.sh
@@ -1,4 +1,4 @@
-#PBS -N moco_RN50_0001
+#PBS -N moco_RN50_001
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=4:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
@@ -14,13 +14,13 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_moco.py \
-  --exp 'RN50_0001' \
+  --exp 'RN50_001' \
   --epochs 100 \
   --print-freq 10 \
   --arch resnet50 \
   --moco-dim 128 \
   --stack_pre_post True \
-  --lr 0.0001 \
+  --lr 0.001 \
   --batch-size 38 \
   --moco-k 65360 \
   --dist-url 'tcp://localhost:10003' --multiprocessing-distributed --world-size 1 --rank 0 \
