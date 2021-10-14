ad90a0b112109828ecb88d561188c12a7f9a5641
KinanZ <kinan.z101@gmail.com>
2021-10-14 11:14:35 +0200

    imagenet finetune

--- job_bash_lincls_mlp_loss.sh
+++ job_bash_lincls_mlp_loss.sh
@@ -1,4 +1,4 @@
-#PBS -N moco_lincl_mlp_RN50_cos_005625_loss
+#PBS -N moco_lincl_mlp_RN18_001_loss
 #PBS -S /bin/bash
 #PBS -l nodes=1:ppn=4:gpus=2:nvidiaMin12GB,mem=16gb,walltime=24:00:00
 #PBS -j oe
@@ -14,14 +14,14 @@ nvidia-smi --query-accounted-apps="pid,gpu_util,mem_util,max_memory_usage,time" 
 
 echo 'Training Should start'
 python3 /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/main_lincls.py \
-  --exp 'moco_lincl_mlp_RN50_cos_005625_loss' \
+  --exp 'moco_lincl_mlp_RN18_001_loss' \
   --epochs 40 \
   --print-freq 1 \
   --optimizer adam \
-  --arch resnet50 \
-  --lr 0.1 \
+  --arch resnet18 \
+  --lr 0.001 \
   --batch-size 48 \
-  --pretrained /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs_2/mlp_RN50_cos_005625/best_model_loss.pth.tar \
-  --dist-url 'tcp://localhost:10002' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --pretrained /misc/student/alzouabk/Thesis/self_supervised_pretraining/moco/outputs_2/mlp_RN18_001_loss/best_model_loss.pth.tar \
+  --dist-url 'tcp://localhost:10005' --multiprocessing-distributed --world-size 1 --rank 0 \
   --workers 8 \
 
